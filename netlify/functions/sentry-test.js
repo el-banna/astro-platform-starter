2d85aeacfb641b94d3641ed76018ec1586718cfe
Mohammad Elbanna <mammelbanna@gmail.com>
2025-07-17 01:55:49 +0400

    Update function

--- netlify/functions/sentry-test.js
+++ netlify/functions/sentry-test.js
@@ -1,7 +1,10 @@
 export default async (request, context) => {
     try {
         const data = JSON.parse(request.body);
-        return Response.json(data);
+        return {
+            statusCode: 200,
+            body: JSON.stringify(data)
+        };
     } catch (error) {
         console.log(error);
         return Response.json({ error: 'Failed fetching data' }, { status: 500 });
