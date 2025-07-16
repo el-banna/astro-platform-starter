dac025bcf9608447ed226d3b00b4f855aa5c29fb
Mohammad Elbanna <mammelbanna@gmail.com>
2025-07-17 02:54:36 +0400

    update sentry function

--- netlify/functions/sentry-test.js
+++ netlify/functions/sentry-test.js
@@ -1,8 +1,22 @@
 export default async (request, context) => {
     try {
         const data = await request.json();
-        console.log(data);
-        return Response.json(data);
+        const sentryEventData = data?.data?.event;
+        const { title, web_url } = sentryEventData || {};
+
+        const payload = {
+            text: `🚨 ${title} 🚨 \n\n${web_url}`
+        };
+        await fetch(
+            'https://chat.googleapis.com/v1/spaces/AAQARDFmfUI/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=dyCoVdTAoW1NxRA0aERTpNGRUYytP1-GI-NeveimY6c',
+            {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify(payload)
+            }
+        );
+
+        return Response.json({});
     } catch (error) {
         console.log(error);
         return Response.json({ error: 'Failed fetching data' }, { status: 500 });
